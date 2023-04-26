85a9c8a03de5417008a3422b753b5e666379c3f3
Mohan K <mohank@tests-iMac.Dlink>
2023-04-26 18:55:11 +0530

    first commit

---
+++ Image/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Image
+//
+//  Created by Mohan K on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   @IBOutlet weak var picture: UIImageView!
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+    
+    @IBOutlet weak var picaso: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
